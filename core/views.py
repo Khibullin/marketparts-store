4b80a5be87774fddfedfc5b1413679456663fb26
Khibullin <youremail@example.com>
2026-04-15 10:48:09 +0500

    restore homepage properly

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from catalog.models import Country, Category, Brand
 
 
 def home(request):
-    return render(request, 'core/home.html')
+    countries = Country.objects.all().order_by('name')
+    categories = Category.objects.all().order_by('name')[:8]
+    brands = Brand.objects.all().order_by('name')[:8]
+
+    return render(request, 'core/home.html', {
+        'countries': countries,
+        'categories': categories,
+        'brands': brands,
+    })
