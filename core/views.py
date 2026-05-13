22ea710bc0fdc12fdd65a8c065585cd90c8ba8f5
Khibullin <youremail@example.com>
2026-05-13 14:00:48 +0500

    Redirect store home to catalog

--- core/views.py
+++ core/views.py
@@ -1,17 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from catalog.models import Country, Category, Brand
 
 
 def home(request):
-    countries = Country.objects.all().order_by('name')
-    categories = Category.objects.all().order_by('name')
-    brands = Brand.objects.all().order_by('name')
-
-    return render(request, 'core/home.html', {
-        'countries': countries,
-        'categories': categories,
-        'brands': brands,
-    })
+    return redirect('catalog_list')
 
 
 def guide(request):
